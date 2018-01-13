fe42b5d0ab8925125ebbefe7e6a9e6b6cff6eb14
Oscar King <zcaboki@ucl.ac.uk>
2018-01-13 12:44:27 +0000

    all tests passing some edits

--- src/lift/MyLiftController.java
+++ src/lift/MyLiftController.java
@@ -8,7 +8,7 @@ package lift;
 public class MyLiftController implements LiftController {
     Instructions instructions = new Instructions();
     int currentFloor = 0;
-    Direction currentDirection = Direction.UNSET;
+    Direction currentDirection = Direction.UP;
     boolean doorsOpen = false;
 
 
@@ -28,9 +28,9 @@ public class MyLiftController implements LiftController {
     }
     
     public synchronized void selectFloor(int floor) throws InterruptedException {
-        instructions.addInstruction(floor,currentDirection);
+        instructions.addInstruction(floor,Direction.UNSET);
         while (!(liftAtFloor(currentFloor,currentDirection)&&doorsOpen&&currentFloor==floor)) wait();
-        instructions.removeInstruction(floor,currentDirection);
+        instructions.removeInstruction(floor,Direction.UNSET);
         notifyAll();
     }
 
@@ -45,12 +45,61 @@ public class MyLiftController implements LiftController {
     public synchronized void doorsOpen(int floor) throws InterruptedException {
         doorsOpen = true;
         notifyAll();
-        wait(10000);
+        wait(5000);
+//        int dir = (currentDirection==Direction.UP) ? 0:1;
+//        int pushCount = instructions.instructionsArr[floor][dir];
+//        while (instructions.instructionsArr[floor][dir]!=0||instructions.instructionsArr[floor][2]!=pushCount) {
+//            wait();
+//        }
         notifyAll();
     }
 
     public synchronized void doorsClosed(int floor) {
     	doorsOpen = false;
-    	notifyAll();
+    }
+
+    //Inner class dealing with instructions
+    private class Instructions {
+        // instructionsArr[floor][0] =
+        private int[][] instructionsArr = new int[9][3];
+
+        private synchronized boolean contains(int floor, Direction direction) {
+            switch (direction) {
+                case UNSET:
+                    return instructionsArr[floor][2] != 0;
+                default:
+                    int dir = (direction == Direction.UP) ? 0:1;
+                    return (instructionsArr[floor][dir]+ instructionsArr[floor][2])!=0;
+            }
+        }
+
+
+        private synchronized void addInstruction(int floor, Direction direction) {
+            switch (direction) {
+                case UP:
+                    instructionsArr[floor][0]++;
+                    break;
+                case DOWN:
+                    instructionsArr[floor][1]++;
+                    break;
+                case UNSET:
+                    instructionsArr[floor][2]++;
+                    break;
+            }
+        }
+
+        private synchronized void removeInstruction(int floor, Direction direction) {
+            switch (direction) {
+                case UP:
+                    instructionsArr[floor][0]=(instructionsArr[floor][0]>0) ? instructionsArr[floor][0]--: instructionsArr[floor][0];
+                    break;
+                case DOWN:
+                    instructionsArr[floor][1]=(instructionsArr[floor][1]>0) ? instructionsArr[floor][1]--: instructionsArr[floor][1];
+                    break;
+                case UNSET:
+                    instructionsArr[floor][2]=(instructionsArr[floor][2]>0) ? instructionsArr[floor][2]--: instructionsArr[floor][2];
+                    break;
+            }
+        }
     }
 }
